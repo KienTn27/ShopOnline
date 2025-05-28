df30ddd5dacc4b0f97faeeaf4c6ccb90c767c4cb
KienTN27 <tranngockien27082004@gmail.com>
2025-05-28 08:05:45 +0700

    Update product

--- src/java/model/Product.java
+++ src/java/model/Product.java
@@ -9,5 +9,92 @@ package model;
  * @author X1 carbon Gen6
  */
 public class Product {
-    
+
+    private int productId;
+    private String name;
+    private String categoryId;
+    private String description;
+    private double price;
+    private int quantity;
+    private String imageUrl;
+    private boolean isActive;
+
+    public Product() {
+    }
+
+    public Product(int productId, String name, String categoryId, String description, double price, int quantity, String imageUrl, boolean isActive) {
+        this.productId = productId;
+        this.name = name;
+        this.categoryId = categoryId;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.imageUrl = imageUrl;
+        this.isActive = isActive;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
 }
