7d04fda8ed5595f2dda5c400f93214569f4c01cd
Shopcuatoi <boomerzz689@gmail.com>
2025-05-28 11:11:23 +0700

    Update Review.java and ReviewDao

--- src/java/dao/ReviewDAO.java
+++ src/java/dao/ReviewDAO.java
@@ -4,10 +4,45 @@
  */
 package dao;
 
-/**
- *
- * @author X1 carbon Gen6
- */
+import dao.DBContext;
+import model.Review;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReviewDAO {
-    
+
+    public List<Review> getAllReviews() {
+        List<Review> list = new ArrayList<>();
+     String sql = """
+    SELECT r.ReviewID, u.FullName, p.Name AS ProductName, r.Comment, r.Rating, r.CreatedAt
+    FROM Reviews r
+    JOIN Users u ON r.UserID = u.UserID
+    JOIN Products p ON r.ProductID = p.ProductID
+    ORDER BY r.CreatedAt DESC
+    """;
+
+
+        try (Connection conn = DBContext.getInstance().getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                list.add(new Review(
+                        rs.getInt("ReviewID"),
+                        rs.getString("FullName"),
+                        rs.getString("ProductName"),
+                        rs.getString("Comment"),
+                        rs.getInt("Rating"),
+                        rs.getTimestamp("CreatedAt")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
+
