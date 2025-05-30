976a810ebe71399ebb7ec24370858072b1b4309c
KienTN27 <tranngockien27082004@gmail.com>
2025-05-30 08:27:31 +0700

    update don hang

--- src/java/dao/OrderDetailDAO.java
+++ src/java/dao/OrderDetailDAO.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package dao;
 
 import java.sql.Connection;
@@ -7,6 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 import model.OrderDetail;
 
+/**
+ *
+ * @author X1 carbon Gen6
+ */
 public class OrderDetailDAO {
 
     public List<OrderDetail> getOrderDetailsByOrderId(int orderId) {
